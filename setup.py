0d2d4f7f07317bfb637c094aef49997e46df2989
Luis Travaglini <lftravaglini@gmail.com>
2026-04-24 09:06:31 -0300

    Codigo atualizado

--- setup.py
+++ setup.py
@@ -1,78 +1,94 @@
-def menu():
+opcao = 0
+while opcao == 0: 
     print("\n=== URNA ELETRÔNICA ===")
     print("1 - Gerenciamento")
     print("2 - Votação")
 
-while True:
-    menu()
-    opcao = input("Escolha uma opção: ")
+    opcao = int(input("Selecione uma opção: "))
 
-    match opcao:
-        case "1":
-            print("=== OPÇÕES DE GERENCIAMENTO ===")
-            print("1 - Candidato.")
-            print("2 - Eleitor")
+while opcao == 1:
+    print("=== OPÇÕES DE GERENCIAMENTO ===")
+    print("1 - Candidato.")
+    print("2 - Eleitor")
+    print("3 - Voltar")
 
-            gerenciamento = input("Sua opção de gerenciamento : ")
+    gerenciamento = int(input("Selecione uma opção: "))
+    if gerenciamento == 3:
+        opcao = 0
+        
+    while gerenciamento == 1:
 
-            match gerenciamento:
-                case "1":
-                    print("=== OPÇÕES DO CANDIDATO ===")
-                    print("1 - Cadastrar Candidato")
-                    print("2 - Editar informações")
-                    print("3 - Excluir Candidato")
-                    print("4 - voltar") 
+        print("=== OPÇÕES DO CANDIDATO ===")
+        print("1 - Cadastrar Candidato")
+        print("2 - Editar informações")
+        print("3 - Excluir Candidato")
+        print("4 - voltar") 
 
-                    candidato = input("Selecione uma opção: ")
+        opc_candidato = int(input("Selecione uma opção: "))
 
-                    match candidato:
-                        case 1:
-                            print("")
-                        case 2:
-                            print("")
-                        case 3:
-                            print("")
-                        case 4: 
-                            menu()
+        match opc_candidato:
+            case 1:
+                print("...")
+            case 2:
+                print("...")
+            case 3:
+                print("...")
+            case 4: 
+                gerenciamento = 0
+                opcao = 1
+    
 
-                case "2":
-                    print("=== OPÇÕES DO ELEITOR ===")
-                    print("1 - lista de eleitores")
-                    print("2 - Cadastro(Novo eleitor)")
-                    print("3 - voltar")
+    while gerenciamento == 2:
 
-                    eleitor = input("Selecione uma opção: ")
+        print("=== OPÇÕES DO ELEITOR ===")
+        print("1 - lista de eleitores")
+        print("2 - Cadastro(Novo eleitor)")
+        print("3 - voltar")
 
-                    match eleitor:
-                        case "1":
-                            print("1 - Editar Eleitores")
-                            print("2 - Buscar Eleitor por CPF ou Titulo")
-                        case "2":
-                            Cpf = input("DIgite seu CPF: ")
+        eleitor = int(input("Selecione uma opção: "))
+
+        match eleitor:
+            case 1:
+                print("1 - Editar Eleitores")
+                print("2 - Buscar Eleitor por CPF ou Titulo")
+            case 2:
+                Cpf = input("DIgite seu CPF: ")
+            case 3:
+                gerenciamento = 0
+                opcao = 1
 
 
+while opcao == 2:
 
+    print("=== OPÇÕES DE VOTAÇÃO ===")
+    print("1 - Listar candidatos")
+    print("2 - Sistema de votação")
+    print("3 - Voltar")
 
-        case "2":
-            print("1 - Listar candidatos")
-            print("2 - Buscar Candidatos")
-            print("3 - Sistema de votação")
+    votacao = int(input("Selecione uma opção: "))
 
-            votacao = input("Selecione uma opção: ")
-            match votacao:
-                case "1": 
-                    pass
-                    #lista de candidatos (?)
-                case "2": 
-                    pass
-                    #Pesquisa de candidatos (?)
-                case "3":
-                    pass
-                    
+    while votacao == 1:
+        print ("Kaiky - 13")
+        print ("Luis - 25")
+        print ("Jõao - 17")
+        votacao = 0
+        opcao = 2
 
-        case "3":
-            print("Encerrando...")
-            break
+    while votacao == 2:
+            
+        print ("1 - Iniciar Votação") 
+        print ("2 - Auditoria")
+        print ("3 - Resultado")
 
-        case _:
-            print("opção inválida!")
+        sist_votacao = int(input("Selecione uma opção: "))
+
+        match sist_votacao:
+            case 1:
+                print ("Iniciando Votação...")
+            case 2: 
+                print ("Iniciando Auditoria...")
+            case 3:
+                print ("Resultados...")
+            case 4:
+                votacao = 0
+                opcao = 2
