77f4dbcb2087ce33f5f4d6da96a70642d2194e8f
Luis Travaglini <lftravaglini@gmail.com>
2026-04-26 19:51:12 -0300

    conexao atulizada com funcao

--- conexao.py
+++ conexao.py
@@ -1,8 +1,9 @@
 import mysql.connector
 
-conexao = mysql.connector.connect(
-    host="localhost",
-    database="sist_votacao",
-    user="root",
-    password="1234"
-)
+def get_conexao():
+    return mysql.connector.connect(
+        host="localhost",
+        database="sist_votacao",
+        user="root",
+        password="1234"
+    )
